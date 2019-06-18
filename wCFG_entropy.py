6890075a04b2b5b0094706a7c2d2856a4faf2f1d
diego <dvelozad@unal.edu.co>
2019-06-18 18:48:34 -0500

    Need for a optimization

--- wCFG_entropy.py
+++ wCFG_entropy.py
@@ -1,71 +1,174 @@
 import numpy as np
 from scipy.special import digamma, factorial
 from scipy.misc import derivative
+import matplotlib.pyplot as plt
 
-entropy_sigma = []
-entropy_os = []
-ed = 100000000
-
-N = 10
+ed_=[1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1,100,1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000,100000000000]
+#ed_=[1e-7,1e-6,1e-5,1e-4,10000,100000,1000000,10000000]
+#N = 20
 T = 27
 
 Num_grammars = 80
 
-for grammar in range(Num_grammars):
-        directory = 'samples/sentences/sentence_samples_ed.'+str(ed)+'/sentence_samples_N.'+str(N)
+counter = 0
+for N in [10,20]:
+        Hs_k1 = []
+        Hd_k1 = []
+        Hs_bar_k1 = []
+        Hd_bar_k1 = []
+        Hs_k2 = []
+        Hd_k2 = []
+        Hs_bar_k2 = []
+        Hd_bar_k2 = []
+        for ed in ed_:
+                entropy_sigma_k1 = []
+                entropy_os_k1 = []
+                entropy_sigma_k2 = []
+                entropy_os_k2 = []        
+                #k = 1
+                for grammar in range(Num_grammars):
+                        directory = 'samples/sentences/sentence_samples_ed.'+str(ed)+'/sentence_samples_N.'+str(N)
 
-        N_sigma = np.load(directory+'/repeat_sigma_sentence_grammar.'+str(grammar)+'.npy')
-        N_o_ = np.load(directory+'/repeat_os_sentence_grammar.'+str(grammar)+'.npy')
-        lenght_sg_o = np.load(directory+'/lenght_sigma_o_grammar.'+str(grammar)+'.npy')
+                        N_sigma = np.load(directory+'/repeat_sigma_k.1_sentence_grammar.'+str(grammar)+'.npy')
+                        N_o_ = np.load(directory+'/repeat_os_k.1_sentence_grammar.'+str(grammar)+'.npy')
+                        #lenght_sg_o = np.load(directory+'/lenght_sigma_o_grammar.'+str(grammar)+'.npy')
 
-        sum_sigma = 0.
-        for i in range(N):
-                n_i = N_sigma[i]
-                N_sig = np.sum(N_sigma)
-                if N_sigma[i] != 0:
-                        sum_sigma += (n_i/N_sig)*(np.log(N_sig) - digamma(n_i) - (1./((n_i)*(n_i+1)))*(-1.)**(n_i))
-        entropy_sigma.append(sum_sigma / np.log(N))
-        sum_os = 0.
-        for i in range(T):
-                n_i = N_o_[i]
-                N_o = np.sum(N_o_)
-                if N_o_[i] != 0:
-                        sum_os += (n_i/N_o)*(np.log(N_o) - digamma(n_i) - (1./((n_i)*(n_i+1)))*(-1.)**(n_i))
-        entropy_os.append(sum_os / np.log(T))
+                        sum_sigma = 0.
+                        for i in range(N):
+                                n_i = N_sigma[i]
+                                N_sig = np.sum(N_sigma)
+                                if N_sigma[i] != 0:
+                                        sum_sigma += (n_i/N_sig)*(np.log(N_sig) - digamma(n_i) - (1./((n_i)*(n_i+1)))*(-1.)**(n_i))
+                        entropy_sigma_k1.append(sum_sigma / np.log(N))
+                        sum_os = 0.
+                        for i in range(T):
+                                n_i = N_o_[i]
+                                N_o = np.sum(N_o_)
+                                if N_o_[i] != 0:
+                                        sum_os += (n_i/N_o)*(np.log(N_o) - digamma(n_i) - (1./((n_i)*(n_i+1)))*(-1.)**(n_i))
+                        entropy_os_k1.append(sum_os / np.log(T))
+                #k = 2
+                for grammar in range(Num_grammars):
+                        N_test = np.load(directory+'/repeat_sigma_k.1_sentence_grammar.'+str(grammar)+'.npy')
+                        directory = 'samples/sentences/sentence_samples_ed.'+str(ed)+'/sentence_samples_N.'+str(N)
+                        N_sigma = np.load(directory+'/repeat_sigma_k.2_sentence_grammar.'+str(grammar)+'.npy')
+                        N_o_ = np.load(directory+'/repeat_os_k.2_sentence_grammar.'+str(grammar)+'.npy')
+                        #lenght_sg_o = np.load(directory+'/lenght_sigma_o_grammar.'+str(grammar)+'.npy')
 
-#print np.mean(np.array(entropy_sigma))
-#print np.mean(np.array(entropy_os))
+                        sum_sigma = 0.
+                        for i in range(N):
+                                for j in range(N):
+                                        n_i = N_sigma[i][j]
+                                        N_sig = np.sum(N_sigma) +1.
+                                        #N_sig = np.sum(N_test)
+                                        if N_sigma[i][j] != 0:
+                                                sum_sigma += (n_i/N_sig)*(np.log(N_sig) - digamma(n_i) - (1./((n_i)*(n_i+1)))*(-1.)**(n_i))
+                        entropy_sigma_k2.append(0.5*sum_sigma / np.log(N))
+                        sum_os = 0.
+                        for i in range(T):
+                                for j in range(T):
+                                        n_i = N_o_[i][j]
+                                        N_o = np.sum(N_o_) + 1.
+                                        if N_o_[i][j] != 0:
+                                                sum_os += (n_i/N_o)*(np.log(N_o) - digamma(n_i) - (1./((n_i)*(n_i+1)))*(-1.)**(n_i))
+                        entropy_os_k2.append(0.5*sum_os / np.log(T))
 
-entropy_sigma = np.sort(np.array(entropy_sigma))
-entropy_os = np.sort(np.array(entropy_os))
+                #print np.mean(np.array(entropy_sigma))
+                #print np.mean(np.array(entropy_os))
 
-#entropy_sigma = np.unique(entropy_sigma)
-#entropy_os = np.unique(entropy_os)
+                entropy_sigma_k1 = np.unique(np.sort(np.array(entropy_sigma_k1)))
+                entropy_os_k1 = np.unique(np.sort(np.array(entropy_os_k1)))
+                entropy_sigma_k2 = np.unique(np.sort(np.array(entropy_sigma_k2)))
+                entropy_os_k2 = np.unique(np.sort(np.array(entropy_os_k2)))
+                
+                #entropy_sigma = np.unique(entropy_sigma)
+                #entropy_os = np.unique(entropy_os)
 
-#print entropy_sigma[int(len(entropy_sigma)/2.)]
-#print '__________________________________________'
-#print entropy_os[int(len(entropy_os)/2.)]
+                #print entropy_sigma[int(len(entropy_sigma)/2.)]
+                #print '__________________________________________'
+                #print entropy_os[int(len(entropy_os)/2.)]
 
-sigma_20 = 0
-sigma_80 = 0
-os_20 = 0
-os_80 = 0
+                sigma_20 = 0
+                sigma_80 = 0
+                os_20 = 0
+                os_80 = 0
 
-for i in range(len(entropy_sigma)):
-        if i == int(len(entropy_sigma)*0.2):
-                sigma_20 = entropy_sigma[i]
-        elif i == int(len(entropy_sigma)*0.8):
-                sigma_80 = entropy_sigma[i]
-                #print entropy_sigma[i]
-print np.mean(entropy_sigma)
-print sigma_80 - sigma_20
-print '__________________________________________'
+                Hs_prom_k1 = np.mean(entropy_sigma_k1)
+                Hd_prom_k1 = np.mean(entropy_os_k1)
 
-for i in range(len(entropy_os)):
-        if i == int(len(entropy_os)*0.2):
-                os_20 = entropy_os[i]
-        elif i == int(len(entropy_os)*0.8):
-                os_80 = entropy_os[i]
-print np.mean(entropy_os)
-print os_80 - os_20
- 
+                Hs_prom_k2 = np.mean(entropy_sigma_k2)
+                Hd_prom_k2 = np.mean(entropy_os_k2)
+
+
+                for i in range(len(entropy_sigma_k1)):
+                        if i == int(len(entropy_sigma_k1)*0.3):
+                                sigma_20 = entropy_sigma_k1[i]
+                        elif i == int(len(entropy_sigma_k1)*0.7):
+                                sigma_80 = entropy_sigma_k1[i]
+                                #print entropy_sigma[i]
+                print np.mean(entropy_sigma_k1)
+                print sigma_80 - sigma_20
+                print '__________________________________________'
+
+                for i in range(len(entropy_os_k1)):
+                        if i == int(len(entropy_os_k1)*0.3):
+                                os_20 = entropy_os_k1[i]
+                        elif i == int(len(entropy_os_k1)*0.7):
+                                os_80 = entropy_os_k1[i]
+                #print np.mean(entropy_os_k1)
+                #print os_80 - os_20
+
+                Hs_k1.append(Hs_prom_k1)
+                Hd_k1.append(Hd_prom_k1)
+                Hs_bar_k1.append(sigma_80 - sigma_20)
+                Hd_bar_k1.append(os_80 - os_20)
+
+
+
+                for i in range(len(entropy_sigma_k2)):
+                        if i == int(len(entropy_sigma_k2)*0.3):
+                                sigma_20 = entropy_sigma_k2[i]
+                        elif i == int(len(entropy_sigma_k2)*0.7):
+                                sigma_80 = entropy_sigma_k2[i]
+
+                print np.mean(entropy_sigma_k2)
+                print sigma_80 - sigma_20
+                print '----------------------------------------------'
+
+                for i in range(len(entropy_os_k2)):
+                        if i == int(len(entropy_os_k2)*0.3):
+                                os_20 = entropy_os_k2[i]
+                        elif i == int(len(entropy_os_k2)*0.7):
+                                os_80 = entropy_os_k2[i]
+
+                Hs_k2.append(Hs_prom_k2)
+                Hd_k2.append(Hd_prom_k2)
+                Hs_bar_k2.append(sigma_80 - sigma_20)
+                Hd_bar_k2.append(os_80 - os_20)
+
+        ed_d=np.array(ed_)
+
+        s1= open("data_"+str(N)+"_Hs_k1.txt","w+")
+        for i in range(len(ed_)):
+                s1.write(str((ed_[i]+counter*0.5*ed_d[i])*((np.log(N))**2.)/((N)**3.))+'\t'+str(Hs_k1[i])+'\t'+str(Hs_bar_k1[i])+'\n')
+        s2= open("data_"+str(N)+"_Hs_k2.txt","w+")
+        for i in range(len(ed_)):
+                s2.write(str((ed_[i]+counter*0.5*ed_d[i])*((np.log(N))**2.)/((N)**3.))+'\t'+str(Hs_k2[i])+'\t'+str(Hs_bar_k2[i])+'\n')
+        d1= open("data_"+str(N)+"_Hd_k1.txt","w+")
+        for i in range(len(ed_)):
+                d1.write(str((ed_[i]+counter*0.5*ed_d[i])*((np.log(N))**2.)/((N)**3.))+'\t'+str(Hd_k1[i])+'\t'+str(Hd_bar_k1[i])+'\n')
+        d2= open("data_"+str(N)+"_Hd_k2.txt","w+")
+        for i in range(len(ed_)):
+                d2.write(str((ed_[i]+counter*0.5*ed_d[i])*((np.log(N))**2.)/((N)**3.))+'\t'+str(Hd_k2[i])+'\t'+str(Hd_bar_k2[i])+'\n')
+        s1.close()
+        s2.close()
+        d1.close()
+        d2.close()
+
+        plt.errorbar(ed_+counter*0.5*ed_d,Hd_k1,yerr=Hd_bar_k1)
+        plt.xscale('log')
+        plt.errorbar(ed_+counter*0.5*ed_d,Hd_k2,yerr=Hd_bar_k2)
+        plt.xscale('log')
+        counter += 1
+
+plt.show()
--- wCFG_samplig.py
+++ wCFG_samplig.py
@@ -10,116 +10,84 @@ def alpha_PG(M,O,M_,O_,ed):
     sd=(1./(N**3.))*(np.sum(np.log(M/M_prom)**2.))
     ss=(1./(N*T))*(np.sum(np.log(O/O_prom)**2.))
     J = np.exp(-np.sum(np.log(M_))-np.sum(np.log(O_))+np.sum(np.log(M))+np.sum(np.log(O)))
-    #ed=(N**3.)/(2.*sd)
-    #es=N*T/(2.*ss)
     es=(N*T)*0.01
 
     sd_=(1./(N**3.))*(np.sum(np.log(M_/M_prom)**2.))
     ss_=(1./(N*T))*(np.sum(np.log(O_/O_prom)**2.))
-    #ed_=(N**3.)/(2.*sd_)
-    #es_=N*T/(2.*ss_)
     ed_=ed
     es_=(N*T)*0.01
     return J*np.exp((-es_*ss_-ed_*sd_+es*ss+ed*sd))
 
 N=10
 T=27
-ed=10000000
 
-N_metropolis=2000
-N_samples=120
-
-M = np.ones((N,N,N))
-O = np.ones((N,T))
-#Normalization
-for i in range(N):
-        M[i,:,:]/=np.sum(M,axis=(1,2))[i]
-for i in range(N):
-        O[i,:]/=np.sum(O,axis=1)[i]
-M_new = np.copy(M)
-O_new = np.copy(O)
-#//
-#M=np.random.normal(0,1,(N,N,N))
-#M = np.abs(M)
-#Normalization
-#for i in range(N):
-#        M[i,:,:]/=np.sum(M,axis=(1,2))[i]
-
-#O=np.random.normal(0,1,(N,T))
-#O = np.abs(O)
-#Normalization
-#for i in range(N):
-#        O[i,:]/=np.sum(O,axis=1)[i]
+ed_ = [10000,100000,1000000,10000000,100000000,1000000000,10000000000,100000000000]
 
 
+N_metropolis=400
+N_samples=100
 
-#for i in range(N):
-#        for j in range(N):
-#                for k in range(N):
-#                        M_new[i][j][k] = np.random.normal(M[i][j][k],1.)
-#M_new = np.abs(M_new)
-#Normalization
-#for i in range(N):
-#        M_new[i,:,:]/=np.sum(M_new,axis=(1,2))[i]
-
-#for i in range(N):
-#        for j in range(T):
-#                O_new[i][j] = np.random.normal(O[i][j],1.)
-#O_new = np.abs(O_new)
-#Normalization
-#for i in range(N):
-#        O_new[i,:]/=np.sum(O_new,axis=1)[i]
-
-counter = 0
-
-directory = 'samples/grammars/grammar_samples_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)
-if not os.path.exists('samples'):
-    os.makedirs('samples')
-if not os.path.exists('samples/grammars'):
-    os.makedirs('samples/grammars')
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-for n_step in range(N_metropolis):
-        for i in range(N):
-                for j in range(N):
-                        for k in range(N):
-                                M_temp = np.copy(M)
-                                m_new_ijk = np.random.normal(M[i][j][k],1.)
-                                m_new_ijk = np.abs(m_new_ijk)
-                                M_temp[i][j][k] = m_new_ijk
-                                if np.random.rand() < alpha_PG(M,O,M_temp,O,ed):
-                                        M_new[i][j][k]=m_new_ijk
-        for i in range(N):
-                for j in range(T):
-                        O_temp = np.copy(O)
-                        o_new_ijk = np.random.normal(O[i][j],1.)
-                        o_new_ijk = np.abs(o_new_ijk)
-                        O_temp[i][j] = o_new_ijk
-
-                        if np.random.rand() < alpha_PG(M,O,M,O_temp,ed):
-                                O_new[i][j]=o_new_ijk
+for ed in ed_:
+        M = np.ones((N,N,N))
+        O = np.ones((N,T))
         #Normalization
         for i in range(N):
-                M_new[i,:,:]/=np.sum(M_new,axis=(1,2))[i]
-        #Normalization
+                M[i,:,:]/=np.sum(M,axis=(1,2))[i]
         for i in range(N):
-                O_new[i,:]/=np.sum(O_new,axis=1)[i]
+                O[i,:]/=np.sum(O,axis=1)[i]
+        M_new = np.copy(M)
+        O_new = np.copy(O)
 
-        M = np.copy(M_new)
-        O = np.copy(O_new)
+        counter = 0
 
-        if n_step >= N_metropolis-500:
-                np.save(directory+'/wCFG_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)+'_sample.'+str(counter)+'_M',M)
-                np.save(directory+'/wCFG_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)+'_sample.'+str(counter)+'_O',O)
-                counter += 1
-        if counter == N_samples:
-                break
-        print n_step
-#Fill the missing samples
-if counter != N_samples:
-        print 'Filling...'
-        for i in range(counter,N_samples):
+        directory = 'samples/grammars/grammar_samples_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)
+        if not os.path.exists('samples'):
+                os.makedirs('samples')
+        if not os.path.exists('samples/grammars'):
+                os.makedirs('samples/grammars')
+        if not os.path.exists(directory):
+                os.makedirs(directory)
+
+        for n_step in range(N_metropolis):
+                for i in range(N):
+                        for j in range(N):
+                                for k in range(N):
+                                        M_temp = np.copy(M)
+                                        m_new_ijk = np.random.normal(M[i,j,k],1.)
+                                        m_new_ijk = np.abs(m_new_ijk)
+                                        M_temp[i,j,k] = m_new_ijk
+                                        if np.random.rand() < alpha_PG(M,O,M_temp,O,ed):
+                                                M_new[i,j,k]=m_new_ijk
+                for i in range(N):
+                        for j in range(T):
+                                O_temp = np.copy(O)
+                                o_new_ijk = np.random.normal(O[i,j],1.)
+                                o_new_ijk = np.abs(o_new_ijk)
+                                O_temp[i,j] = o_new_ijk
+
+                                if np.random.rand() < alpha_PG(M,O,M,O_temp,ed):
+                                        O_new[i,j]=o_new_ijk
+                #Normalization
+                for i in range(N):
+                        M_new[i,:,:]/=np.sum(M_new,axis=(1,2))[i]
+                #Normalization
+                for i in range(N):
+                        O_new[i,:]/=np.sum(O_new,axis=1)[i]
+
+                M = np.copy(M_new)
+                O = np.copy(O_new)
+                print counter
+                if n_step >= N_metropolis-100:
                         np.save(directory+'/wCFG_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)+'_sample.'+str(counter)+'_M',M)
                         np.save(directory+'/wCFG_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)+'_sample.'+str(counter)+'_O',O)
                         counter += 1
+                if counter == N_samples:
+                        break
+                print n_step
+        #Fill the missing samples
+        if counter != N_samples:
+                print 'Filling...'
+                for i in range(counter,N_samples):
+                                np.save(directory+'/wCFG_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)+'_sample.'+str(counter)+'_M',M)
+                                np.save(directory+'/wCFG_N.'+str(N)+'_T.'+str(T)+'_ed.'+str(ed)+'_sample.'+str(counter)+'_O',O)
+                                counter += 1
